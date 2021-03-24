e42c5cc9cb07b52374c6fe41e39a8e8e6e11ce96
Matt Gross <mbgross111@gmail.com>
2021-03-24 15:01:18 -0700

    Relate Users and Flowers models

--- models/index.js
+++ models/index.js
@@ -3,6 +3,23 @@ const Flowers = require('./Flowers')
 const Messages = require('./Messages')
 const Flags = require('./Flags')
 
-// Add sequelize Table Associations
+// Users -> Flowers relationships
+Users.belongsToMany(User, {
+  through: Flowers,
+  foreignKey: 'sender_id',
+  as: 'sent_flowers_to'
+});
+
+Users.belongsToMany(User, {
+  through: Flowers,
+  foreignKey: 'recipient_id',
+  as: 'received_flowers_from'
+});
+
+Users.hasMany(Flowers, {foreignKey: 'sender_id'});
+Users.hasMany(Flowers, {foreignKey: 'recipient_id'});
+
+Flowers.belongsTo(Users, {foreignKey: 'sender_id'});
+Flowers.belongsTo(Users, {foreignKey: 'recipient_id'});
 
 module.exports = { Users, Flowers, Messages, Flags };
