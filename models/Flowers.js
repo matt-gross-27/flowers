4a0a4e333cc647f0fcee5e3f1ea8bd35269b1281
Matt Gross <mbgross111@gmail.com>
2021-03-23 18:01:48 -0700

    add models Flowers, Messages and Flags

---
+++ models/Flowers.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Flowers extends Model {}
+
+Flowers.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    sender_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    recipient_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      } 
+    }
+  },
+  {
+    sequelize,
+    modelName: 'flowers',
+  });
+
+module.exports = Flowers
---
+++ models/Messages.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Messages extends Model {}
+
+Messages.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    message_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sender_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    recipient_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      } 
+    }
+  },
+  {
+    sequelize,
+    modelName: 'messages',
+  });
+
+module.exports = Messages
