c6cfbad638ee1cac0cf3eaab52880af567cf3faa
echo <miyunniecho@gmail.com>
2021-03-24 19:49:51 -0700

    separate branch for handlebars

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,32 @@
 const router = require('express').Router();
+const { Users, Messages, Flowers, Flags} = require('../../models'); 
 
+
+//getting all users
+router.get('/', (req, res) => {
+    Users.findAll({
+        attributes: {exclude: ['passowrd']}
+    }).then(dbUserdata => res.json(dbUserdata))
+    .catch(err => {
+        console.log(err); 
+        res.status(500).json(err); 
+    });
+});
+
+//get specific user by id 
+router.get('/:id', (req, res) => {
+    Users.findOne({
+        attributes: {exclude : ['password']}, 
+        where: {
+            id: req.params.id
+        }, 
+        include: [
+            {
+                model: Messages, 
+                attributes: []
+            }
+        ]
+    })
+})
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -4,5 +4,11 @@ const Messages = require('./Messages')
 const Flags = require('./Flags')
 
 // Add sequelize Table Associations
+Messages.belongsTo(Users);
+Users.hasMany(Messages); 
+Users.hasMany(Flowers);
+Flowers.belongsTo(Users); 
+Users.hasMany(Flags); 
+Flags.belongsTo(Users); 
 
 module.exports = { Users, Flowers, Messages, Flags };
