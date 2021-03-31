d1428b82efab7a452a79bcbe75d3736dfb4b26ec
Matt Gross <mbgross111@gmail.com>
2021-03-30 23:40:20 -0700

    debug home route /:id

--- server.js
+++ server.js
@@ -41,17 +41,7 @@ app.use(routes);
 // turn on seedDatabase and set { force: true } to user seed data
 const seedDatabase = require('./db/seeds');
 
-<<<<<<< HEAD
-const force = false;
-
-sequelize.sync({ force }).then(() => {
-    if (force) {
-        seedDatabase();
-    }
-    app.listen(PORT, () => console.log(`application live at http://localhost:${PORT}/`));
-=======
 sequelize.sync({ force: true }).then(() => {
   seedDatabase();
   app.listen(PORT, () => console.log(`application live at http://localhost:${PORT}/`));
->>>>>>> 720935129c383deba49b0f3a7b375a1eb9eaef2c
 });
