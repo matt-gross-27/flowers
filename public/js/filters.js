967e2eb8a4f5cdc941e2ca84303468dc47529ca6
Matt Gross <mbgross111@gmail.com>
2021-04-01 18:20:06 -0700

    debug filter.js and add features => only show users who are interested in my gender, filter out users who have blocked/flagged me, filter out users whom i have blocked/flagged, or already send flowers to. Alert user if they get a match and redirect them to dashboard

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,126 +1,156 @@
-$(document).ready(function() {});
+$(document).ready(function () { });
+
 let savedUsers;
 
+// HANDLE BARS TEMPLATE FOR ONE CARD
 const hbTemplate = Handlebars.compile(`
-<div class="col-sm-6 col-md-4">
-  <div class="card mb-3 user-card" data-user="{{id}}">
-    <img src="{{profile_picture_src}}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">{{first_name}} {{last_name}}, {{age}}</h5>
-      <p class="card-text">{{description}}</p>
-      {{#if sentFlowers}}
-      <em>Flowers sent</em>
-      {{else}}
-      <button data-attribute-id="{{id}}" id="send-flowers-{{id}}"
-              class="btn btn-primary dashboard-btn send-flowers-btn" title="Send Flowers">Send Flowers 🌹</button>
-      {{/if}}
+  <div class="col-12 col-md-6 col-lg-4">
+    <div class="card mb-3 user-card" data-user="{{id}}">
+      <img class="search-img" src="{{profile_picture_src}}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <div class="card-body-text">
+
+        <h5 class="card-title">{{first_name}} {{last_name}}, {{age}}</h5>
+        <p class="card-text">{{description}}</p>
+
+        </div>
+        <button data-attribute-id="{{id}}" id="send-flowers-{{id}}"
+                class="btn btn-primary mt-3 w-100 send-flowers-btn" title="Send Flowers"
+        >
+          Send Flowers 🌹
+        </button>
+      </div>
     </div>
-  </div>
-</div>`);
+  </div>`
+);
 
 let currentUser;
 
 function loadPreferences() {
-    $('input:checkbox').each(function() {
-        $(this).attr('checked', currentUser[$(this).attr('id')])
-    })
+  $('input:checkbox').each(function () {
+    $(this).attr('checked', currentUser[$(this).attr('id')])
+  })
 }
 
 (async function init() {
-    const user = await $.get('/api/users/current-user');
-    currentUser = user;
+  const user = await $.get('/api/users/current-user');
+  currentUser = user;
 
-    loadPreferences();
-    showFilteredUsers();
+  loadPreferences();
+  showFilteredUsers();
 })()
 
 const flowersSent = user => !currentUser.sent_flowers_to.find(({ id }) => id == user.id);
-const loadUsers = (users) => {
-    savedUsers = users;
-    /*savedUsers = users.map(user => {
-        if (currentUser.sent_flowers_to.find(({ id }) => id == user.id)) {
-            user.sentFlowers = true;
-        }
-        return user;
-    }) */
 
-    const filteredUsers = users
-        /*.filter(flowersSent)*/
-        .map(user => hbTemplate(user))
-    $('.user-list').html(filteredUsers.length ?
-        filteredUsers :
-        'There are no users around you ');
+const renderUsers = async (users) => {
+  savedUsers = users;
+
+  const filteredUsers = users.map(user => hbTemplate(user))
+
+  const loadRes = await $('.user-list').html(filteredUsers.length ? filteredUsers : 'There are no users around you ');
+
+  let clickedId;
+
+  if (loadRes) {
+    // init send flowers buttons
+    $('.send-flowers-btn').click(async function () {
+      clickedId = this.getAttribute('data-attribute-id');
+
+      const fetchRes = await fetch(`/api/users/send-flowers`, {
+        method: 'PUT',
+        body: JSON.stringify({ recipient_id: clickedId }),
+        headers: { 'Content-Type': "application/json" }
+      });
+
+      if (fetchRes.ok) {        
+        const data = await fetchRes.json()
+        const myMatches = data.user_matches.map(obj => obj.id);
+        
+        if (myMatches.includes(parseInt(clickedId))) {
+          alert(`😀🌹😀 Its a Match! 😀🌹😀`)
+          location.replace('/dashboard');
+        } else {
+          showFilteredUsers();
+        }
+      } else {
+        alert(fetchRes.statusText);
+      }
+    })
+  }
 }
 
 async function showFilteredUsers(event) {
-    event && event.preventDefault();
-    const form = $('#filter-form');
+  event && event.preventDefault();
+  const form = $('#filter-form');
 
-    const { latitude, longitude } = currentUser;
+  const { latitude, longitude } = currentUser;
 
-    const filters = form.serialize()
-        .replace(/interested_in_\w/g, 'gender') +
-        `&latitude=${latitude}` +
-        `&longitude=${longitude}`;
+  const filters = form.serialize()
+    .replace(/interested_in_\w/g, 'gender') +
+    `&latitude=${latitude}` +
+    `&longitude=${longitude}`;
 
-    showUsers(filters);
-    saveUserPreferences(form);
+  getUsers(filters);
+  savePreferences(form);
 
-    const modal = $('#exampleModal').modal('hide');
+  $('#exampleModal').modal('hide');
 }
 
 $('#filter-btn').on('click', showFilteredUsers)
 
-async function showUsers(filters) {
-    const url = filters ? `/api/filter?${filters}` : '/api/users'
-    const users = await $.get(url);
+async function getUsers(filters) {
+  const url = filters ? `/api/filter?${filters}` : '/api/users'
+  let users = await $.get(url);
 
-    loadUsers(users);
+  // filter out users whom I have sent flowers/blocks/flags or sent me blocks/flags
+  users = users.filter(p => {
+    return [
+      ...p.sent_block_to,
+      ...p.received_block_from,
+      ...p.sent_flag_to,
+      ...p.received_flag_from,
+      ...p.received_flowers_from,
+    ]
+      .map(u => u.id)
+      .includes(currentUser.id) === false
+  });
+
+  // only show users who are interested in my gender
+  
+  users = users.filter(p => {
+    return p[`interested_in_${currentUser.gender}`] === true
+  });
+
+  renderUsers(users);
 }
 
-async function saveUserPreferences(form) {
-    const data = form.serializeArray()
-        .reduce((acum, { name, value }) => {
-            try {
-                acum[name] = JSON.parse(value);
-            } catch (error) {
-                acum[name] = value;
-            }
-            return acum;
-        }, {});
+async function savePreferences(form) {
+  const data = form.serializeArray()
+    .reduce((acum, { name, value }) => {
+      try {
+        acum[name] = JSON.parse(value);
+      } catch (error) {
+        acum[name] = value;
+      }
+      return acum;
+    }, {});
 
-    const resp = await $.ajax({
-        url: '/api/users',
-        method: 'PUT',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            id: currentUser.id,
-            ...data,
-            interested_in_m: Boolean(data['interested_in_m']),
-            interested_in_f: Boolean(data['interested_in_f']),
-            interested_in_o: Boolean(data['interested_in_o']),
-        }),
-    });
+  const resp = await $.ajax({
+    url: '/api/users',
+    method: 'PUT',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      id: currentUser.id,
+      ...data,
+      interested_in_m: Boolean(data['interested_in_m']),
+      interested_in_f: Boolean(data['interested_in_f']),
+      interested_in_o: Boolean(data['interested_in_o']),
+    }),
+  });
 
-    currentUser = resp.user;
+  currentUser = resp.user;
 }
 
-$('[name=distance]').on('input', function() {
-    $('#distance').html($(this).val() + ' miles');
+$('[name=distance]').on('input', function () {
+  $('#distance').html($(this).val() + ' miles');
 })
-
-// $(document).on('click', '.send-flowers ', async function(event) {
-//     event.preventDefault();
-
-//     // const recipient_id = $(this).closest('.user-card').data('user');
-//     const recipient_id = $(this).attr("value");
-//     console.log(recipient_id);
-
-//     currentUser = await $.ajax({
-//         url: '/api/users/send-flowers',
-//         method: 'PUT',
-//         data: JSON.stringify(recipient_id)
-//     });
-
-//     loadUsers(savedUsers);
-// });
